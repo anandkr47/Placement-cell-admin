9a0044dbce9c6afded65049bcec4815b7b8d474b
Anand kumar <anandkumar.triden@gmail.com>
2023-05-28 16:57:37 +0530

    update

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path'); // Add the path module
 const profileController = require('../controllers/profile_controller');
