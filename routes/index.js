69abbb7edbab86347310c5c8ecf631711d1a1b2d
Anand kumar <anandkumar.triden@gmail.com>
2023-06-02 23:38:18 +0530

    updae index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ console.log("router reloaded");
 router.use("/", require("./users"));
 
 router.use("/student", require("./students"));
-router.use("/interview", require("./interviews"));
+//router.use("/interview", require("./interviews"));
 
 
 module.exports = router;
